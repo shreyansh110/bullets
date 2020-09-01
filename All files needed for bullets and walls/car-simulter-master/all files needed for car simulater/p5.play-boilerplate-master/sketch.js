67f4796cee5657bb663df5ab5e776b1b127a492a
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2020-09-01 13:57:02 +0530

    Add files via upload

---
+++ All files needed for bullets and walls/car-simulter-master/all files needed for car simulater/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,40 @@
+var car,bullet;
+var wall;
+var thickness,weight,speed;
+var damage=0;
+
+
+
+
+function setup() {
+  createCanvas(1000,400);
+ car=createSprite(950,200,50,50);
+ wall=createSprite(10,200,10,400);
+ bullet=createSprite(908,200,30,20)
+speed=random(223,321);
+thickness=random(22,83);
+weight=random(30,52);
+
+ }
+
+ function draw() {
+  background(80,500,255);  
+  bullet.velocityX=-8;
+
+ if(bullet.x-wall.x<wall.width/2+bullet.width/2){
+ bullet.velocityX=0;
+ damage=0.5*weight*speed*speed/thickness*thickness*thickness;
+ }
+
+if(damage<10&&bullet.x-wall.x<wall.width/2+bullet.width/2){
+wall.shapeColor="green";
+}
+
+if(damage>10&&bullet.x-wall.x<wall.width/2+bullet.width/2){
+  wall.shapeColor="red";
+}
+
+
+ drawSprites();
+  
+ }
